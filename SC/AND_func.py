5e0a3351277ac2b5657144abb13ac3bb02c5b20c
AkhilKushe <kusheakhil@gmail.com>
2021-09-27 10:19:26 +0530

    Basic simulations added

---
+++ SC/AND_func.py
@@ -0,0 +1,14 @@
+
+
+x1, x2 = map(int, input("Enter x1 and x2 values space seperated : ").split())
+
+w1 = 1
+w2 = 1
+net = x1*w1 + x2*w2
+
+print("Net = ", net)
+print("Output = ", 1 if net >= 2 else 0)
+
+input("Press ener to exit")
+
+
