765320dbb542f5ec60457f9c658f610ae9264888
AkhilKushe <kusheakhil@gmail.com>
2021-09-27 11:19:23 +0530

    fixed plot not showing error and modified AND example

--- SC/AND_func.py
+++ SC/AND_func.py
@@ -1,13 +1,14 @@
 
 
-x1, x2 = map(int, input("Enter x1 and x2 values space seperated : ").split())
+x1, x2 = map(int, input("Enter x1 and x2 (space seperated) : ").split())
 
 w1 = 1
 w2 = 1
-net = x1*w1 + x2*w2
+b = -1
+net = x1*w1 + x2*w2 + b
 
-print("Net = ", net)
-print("Output = ", 1 if net >= 2 else 0)
+
+print("Output = ", 1 if net >= 0 else 0)
 
 input("Press ener to exit")
 
